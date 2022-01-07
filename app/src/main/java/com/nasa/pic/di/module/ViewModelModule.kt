42835b6ced3df9afea7e820964544c6182d2d652
Bala <bala2explore@gmail.com>
2022-01-07 10:36:23 +0530

    Add: NasaViewModel, update ViewModelModule

--- app/src/main/java/com/nasa/pic/di/module/ViewModelModule.kt
+++ app/src/main/java/com/nasa/pic/di/module/ViewModelModule.kt
@@ -1,8 +1,21 @@
 package com.nasa.pic.di.module
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.nasa.pic.di.ViewModelKey
+import com.nasa.pic.presentation.viewmodel.NasaViewModel
+import com.nasa.pic.presentation.viewmodel.ViewModelFactory
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @Module
 abstract class ViewModelModule {
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
 
+    @Binds
+    @IntoMap
+    @ViewModelKey(NasaViewModel::class)
+    abstract fun nasaApodListViewModel(viewModelNasa: NasaViewModel): ViewModel
 }
---
+++ app/src/main/java/com/nasa/pic/presentation/viewmodel/NasaViewModel.kt
@@ -0,0 +1,11 @@
+package com.nasa.pic.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Singleton
+
+@Singleton
+class NasaViewModel :
+    ViewModel() {
+
+
+}
