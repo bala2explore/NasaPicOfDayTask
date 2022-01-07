2d81dfb67adab17e9164e673268462a3ab368705
Bala <bala2explore@gmail.com>
2022-01-07 11:18:10 +0530

    Update: Updating room db provider in dbmodule

--- app/src/main/java/com/nasa/pic/di/module/NasaDbModule.kt
+++ app/src/main/java/com/nasa/pic/di/module/NasaDbModule.kt
@@ -1,8 +1,23 @@
 package com.nasa.pic.di.module
 
+import android.content.Context
+import androidx.room.Room
+import com.nasa.pic.data.local.NasaDao
+import com.nasa.pic.data.local.NasaDb
+import com.nasa.pic.di.qualifier.ApplicationContext
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class NasaDbModule {
+    @Provides
+    @Singleton
+    fun provideDb(@ApplicationContext context: Context): NasaDb = Room
+        .databaseBuilder(context, NasaDb::class.java, "Nasaapp.db")
+        .build()
 
+    @Provides
+    @Singleton
+    fun provideDao(nasaDb: NasaDb): NasaDao = nasaDb.NasaDao()
 }
