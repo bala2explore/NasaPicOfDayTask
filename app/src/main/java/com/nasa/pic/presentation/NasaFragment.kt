7a5d307695393cd42f54af397a6eef23af800ddf
Bala <bala2explore@gmail.com>
2022-01-07 10:39:56 +0530

    Update: Map NasaViewModel in NasaFragment

--- app/src/main/java/com/nasa/pic/presentation/NasaFragment.kt
+++ app/src/main/java/com/nasa/pic/presentation/NasaFragment.kt
@@ -6,11 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.nasa.pic.NasaApplication
 import com.nasa.pic.R
 import com.nasa.pic.databinding.FragmentNasaBinding
+import com.nasa.pic.presentation.viewmodel.NasaViewModel
+import javax.inject.Inject
 
 class NasaFragment : Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
     companion object {
         fun newInstance() = NasaFragment()
@@ -21,10 +28,11 @@ class NasaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         NasaApplication.instance.getNasaAppComponent().inject(this)
-
+        val nasaViewModel: NasaViewModel by viewModels { viewModelFactory }
         val binding = DataBindingUtil.inflate<FragmentNasaBinding>(
             inflater, R.layout.fragment_nasa, container, false
         )
+
         return binding.root
     }
 
